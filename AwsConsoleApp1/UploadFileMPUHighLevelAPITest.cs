6dc8ca47ea20c01e6859212d877cce3f51823d67
Conrado Moura <cskies@hotmail.com>
2018-07-19 17:30:57 -0300

    initial commit for bucket test on s3

---
+++ AwsConsoleApp1/UploadFileMPUHighLevelAPITest.cs
@@ -0,0 +1,97 @@
+﻿using Amazon.S3;
+using Amazon.S3.Transfer;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+
+namespace Amazon.DocSamples.S3
+{
+    class UploadFileMPUHighLevelAPITest
+    {
+        private const string bucketName = "cf-templates-1d2l4rxgwl9sh-sa-east-1"; //"*** provide bucket name ***";
+        private const string keyName = "keyNameTest"; // "*** provide a name for the uploaded object ***";
+        private const string filePath = @"C:\Users\conrado.moura\Downloads\tmp\s3test\t1.txt";     //"*** provide the full path name of the file to upload ***";
+        // Specify your bucket region (an example region is shown).
+        private static readonly RegionEndpoint bucketRegion = RegionEndpoint.SAEast1; //do not forget to change region
+        private static IAmazonS3 s3Client;
+
+        public static void Main()
+        {
+            s3Client = new AmazonS3Client(bucketRegion);
+            UploadFileAsync().Wait();
+        }
+
+        private static async Task UploadFileAsync()
+        {
+            try
+            {
+                var fileTransferUtility =
+                    new TransferUtility(s3Client);
+
+                // Option 1. Upload a file. The file name is used as the object key name.
+                await fileTransferUtility.UploadAsync(filePath, bucketName);
+                Console.WriteLine("Upload 1 completed");
+
+                // Option 2. Specify object key name explicitly.
+                await fileTransferUtility.UploadAsync(filePath, bucketName, keyName);
+                Console.WriteLine("Upload 2 completed");
+
+                // Option 3. Upload data from a type of System.IO.Stream.
+                using (var fileToUpload =
+                    new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                {
+                    await fileTransferUtility.UploadAsync(fileToUpload,
+                                               bucketName, keyName);
+                }
+                Console.WriteLine("Upload 3 completed");
+
+                // Option 4. Specify advanced settings.
+                var fileTransferUtilityRequest = new TransferUtilityUploadRequest
+                {
+                    BucketName = bucketName,
+                    FilePath = filePath,
+                    StorageClass = S3StorageClass.StandardInfrequentAccess,
+                    PartSize = 6291456, // 6 MB.
+                    Key = keyName,
+                    CannedACL = S3CannedACL.PublicRead
+                };
+                fileTransferUtilityRequest.Metadata.Add("param1", "Value1");
+                fileTransferUtilityRequest.Metadata.Add("param2", "Value2");
+
+                await fileTransferUtility.UploadAsync(fileTransferUtilityRequest);
+                Console.WriteLine("Upload 4 completed");
+
+                Console.ReadKey();
+            }
+            catch (AmazonS3Exception e)
+            {
+                Console.WriteLine("Error encountered on server. Message:'{0}' when writing an object", e.Message);
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unknown encountered on server. Message:'{0}' when writing an object", e.Message);
+                Console.ReadKey();
+            }
+        }
+
+        public static void ProcessDirectory(string targetDirectory)
+        {
+            // Process the list of files found in the directory.
+            string[] fileEntries = Directory.GetFiles(targetDirectory);
+            foreach (string fileName in fileEntries)
+                ProcessFile(fileName);
+
+            // Recurse into subdirectories of this directory.
+            string[] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
+            foreach (string subdirectory in subdirectoryEntries)
+                ProcessDirectory(subdirectory);
+        }
+
+        public static void ProcessFile(string path)
+        {
+            Console.WriteLine("Processed file '{0}'.", path);
+        }
+    }
+}
